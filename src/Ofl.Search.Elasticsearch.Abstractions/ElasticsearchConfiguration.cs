41a21f004d4ae2e3b3d3036b1a23a52abec8dfbc
casperOne <casperOne@caspershouse.com>
2017-04-18 15:21:54 -0400

    Initial migration.

---
+++ src/Ofl.Search.Elasticsearch.Abstractions/ElasticsearchConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Ofl.Search.Elasticsearch
+{
+    public class ElasticsearchConfiguration
+    {
+        public Uri Url { get; set; }
+
+        public bool EnableHttpCompression { get; set; }
+    }
+}
---
+++ src/Ofl.Search.Elasticsearch.Abstractions/IElasticClientFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Nest;
+
+namespace Ofl.Search.Elasticsearch
+{
+    public interface IElasticClientFactory
+    {
+        Task<IElasticClient> CreateClientAsync(Func<ConnectionSettings, ConnectionSettings> connectionSettingsModifier,
+            CancellationToken cancellationToken);
+    }
+}
