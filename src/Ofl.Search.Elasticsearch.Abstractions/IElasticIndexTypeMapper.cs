922a701bde504fa8cb097e6f21407c91f591f126
casperOne <casperOne@caspershouse.com>
2019-01-12 14:05:59 -0500

    Added IElasticIndexTypeMapper.

---
+++ src/Ofl.Search.Elasticsearch.Abstractions/IElasticIndexTypeMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Nest;
+
+namespace Ofl.Search.Elasticsearch
+{
+    public interface IElasticIndexTypeMapper
+    {
+        Type Type { get; }
+
+        IClrTypeMapping DefineMappingForType(ClrTypeMappingDescriptor mapping);
+    }
+}
